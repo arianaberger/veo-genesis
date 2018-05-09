0486becf87066395698896cc4442453c09f957f2
ecssiah <ecssiah@gmail.com>
2018-05-08 20:58:56 -0500

    Add fourth model to each part category

--- app/models/part.rb
+++ app/models/part.rb
@@ -1,7 +1,7 @@
 class Part < ActiveRecord::Base
   belongs_to :being
 
-  HEAD_MODELS = [1, 2, 3]
-  BODY_MODELS = [1, 2, 3]
-  EYES_MODELS = [1, 2, 3]
+  HEAD_MODELS = [1, 2, 3, 4]
+  BODY_MODELS = [1, 2, 3, 4]
+  EYES_MODELS = [1, 2, 3, 4]
 end
