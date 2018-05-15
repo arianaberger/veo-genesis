1a6e0f0cb0a91ffea6ca37487e34aef0f0e3a575
ecssiah <ecssiah@gmail.com>
2018-05-15 13:01:34 -0500

    Fix bug introduced by renaming getElementById

--- public/js/customize_being.js
+++ public/js/customize_being.js
@@ -1,65 +1,65 @@
 function bodyChange(radioObject) {
   var model = radioObject.value;
 
-  document.getElementBymodel("body_preview").src = `/img/body${model}.png`;
+  document.getElementById("body_preview").src = `/img/body${model}.png`;
 };
 
 function bodyColorChange(rangeObject) {
   var angle = Math.round(rangeObject.value * 3.6);
-  var bright = document.getElementBymodel("body_brightness").value / 2 + 50;
+  var bright = document.getElementById("body_brightness").value / 2 + 50;
 
-  document.getElementBymodel("body_preview").style.filter = 
+  document.getElementById("body_preview").style.filter = 
     `brightness(${bright}%) hue-rotate(${angle}deg)`;
 };
 
 function bodyBrightnessChange(rangeObject) {
-  var angle = Math.round(document.getElementBymodel("body_color").value * 3.6)
+  var angle = Math.round(document.getElementById("body_color").value * 3.6)
   var bright = rangeObject.value / 2 + 50;
 
-  document.getElementBymodel("body_preview").style.filter = 
+  document.getElementById("body_preview").style.filter = 
     `brightness(${bright}%) hue-rotate(${angle}deg)`;
 };
 
 function headChange(radioObject) {
   var model = radioObject.value;
 
-  document.getElementBymodel("head_preview").src = `/img/head${model}.png`;
+  document.getElementById("head_preview").src = `/img/head${model}.png`;
 };
 
 function headColorChange(rangeObject) {
   var angle = Math.round(rangeObject.value * 3.6);
-  var bright = document.getElementBymodel("head_brightness").value / 2 + 50;
+  var bright = document.getElementById("head_brightness").value / 2 + 50;
 
-  document.getElementBymodel("head_preview").style.filter = 
+  document.getElementById("head_preview").style.filter = 
     `brightness(${bright}%) hue-rotate(${angle}deg)`;
 };
 
 function headBrightnessChange(rangeObject) {
-  var angle = Math.round(document.getElementBymodel("head_color").value * 3.6)
+  var angle = Math.round(document.getElementById("head_color").value * 3.6)
   var bright = rangeObject.value / 2 + 50;
 
-  document.getElementBymodel("head_preview").style.filter = 
+  document.getElementById("head_preview").style.filter = 
     `brightness(${bright}%) hue-rotate(${angle}deg)`;
 };
 
 function eyesChange(radioObject) {
   var model = radioObject.value;
 
-  document.getElementBymodel("eyes_preview").src = `/img/eyes${model}.png`;
+  document.getElementById("eyes_preview").src = `/img/eyes${model}.png`;
 };
 
 function eyesColorChange(rangeObject) {
   var angle = Math.round(rangeObject.value * 3.6);
-  var bright = document.getElementBymodel("eyes_brightness").value / 2 + 50;
+  var bright = document.getElementById("eyes_brightness").value / 2 + 50;
 
-  document.getElementBymodel("eyes_preview").style.filter = 
+  document.getElementById("eyes_preview").style.filter = 
     `brightness(${bright}%) hue-rotate(${angle}deg)`;
 };
 
 function eyesBrightnessChange(rangeObject) {
-  var angle = Math.round(document.getElementBymodel("eyes_color").value * 3.6)
+  var angle = Math.round(document.getElementById("eyes_color").value * 3.6)
   var bright = rangeObject.value / 2 + 50;
 
-  document.getElementBymodel("eyes_preview").style.filter = 
+  document.getElementById("eyes_preview").style.filter = 
     `brightness(${bright}%) hue-rotate(${angle}deg)`;
 };
